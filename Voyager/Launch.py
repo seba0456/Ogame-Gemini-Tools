3a9c4b18f875a513937d7ff09d725d75c221f91e
Sebastian <46867564+seba0456@users.noreply.github.com>
2022-08-18 22:50:24 +0200

    Update Launch.py

--- Voyager/Launch.py
+++ Voyager/Launch.py
@@ -1,4 +1,4 @@
-mode = input("Should program launch in scanning mode? (Yes or No)"?)
+mode = input("Should program launch in scanning mode? (Yes or No)")
 if mode ==str("Yes"):
     exec(open('Voyager.py').read())
 elif mode ==str("No"):
