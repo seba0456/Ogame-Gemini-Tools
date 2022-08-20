7ec07522caae63a5db387b3089199307ff32cf16
Sebastian <46867564+seba0456@users.noreply.github.com>
2022-08-20 12:58:00 +0200

    Update InstallDependences.sh

--- Gemini/InstallDependences.sh
+++ Gemini/InstallDependences.sh
@@ -1,6 +1,6 @@
 echo This script will install all bot dependences.
 sleep 3
-pip install ogame==8.1.0.21 && pip install tqdm
+pip install ogame==8.1.0.21 && pip install tqdm python3 -m && pip install --upgrade pip && python3 -m pip install --upgrade Pillow
 sleep 6
 echo make sure to edit config.ini
 echo type something when you are ready to start bot
