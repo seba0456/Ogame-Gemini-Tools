c2612e333fc29616801d5ee8e7a859d8fac0a95c
Sebastian <46867564+seba0456@users.noreply.github.com>
2023-01-16 00:44:22 +0100

    Update Launch.py
    -dodano wsparcie do nowego modułu

--- Voyager/Launch.py
+++ Voyager/Launch.py
@@ -9,6 +9,8 @@ while i == 0:
         exec(open('Luna.py').read())
     elif mode ==str("voyager draw"):
         exec(open('LunaDrawer.py').read())
+    elif mode ==str("voyager ranger"):
+        exec(open('Ranger.py').read())
     elif mode ==str("voyager abort"):
         i = 1
     elif mode ==("help"):
@@ -16,6 +18,7 @@ while i == 0:
         print("voyager scout", "   ", "--- Scan universe and save it to file .JSON")
         print("voyager report", "  ", "--- Reads data from .JSON file and show details about player.")
         print("voyager draw", "    ", "--- Reads data from .JSON file and creates universe map")
+        print("voyager ranger", "    ", "--- Reads data from .JSON file and lists most isolated systems")
         print("voyager abort", "   ", "--- Closes program")
         print('_' * 80)
     else:
