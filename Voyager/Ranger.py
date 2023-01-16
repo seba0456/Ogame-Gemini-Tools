fb0d4a3e8be016fdb784eccb486418d854b442aa
Sebastian <46867564+seba0456@users.noreply.github.com>
2023-01-16 13:19:03 +0100

    Update Ranger.py
    -od teraz można ignorować graczy na urlopie
    -naprawiono błąd, który powodował, że program wyświetlał fałszywe informacje

--- Voyager/Ranger.py
+++ Voyager/Ranger.py
@@ -12,6 +12,7 @@ scan_range= int(cfg.get('ranger','scan_range'))
 minimum_rank= int(cfg.get('ranger','minimum_rank'))
 results=int(cfg.get('ranger','results'))
 maximum_rank=int(cfg.get('ranger','maximum_rank'))
+ingore_vacat=int(cfg.get('ranger','ingore_vacat'))
 # Opening JSON file
 a = 1
 while a == 1:
@@ -27,6 +28,7 @@ x=[]
 systems = []
 planets=0
 for gal in range(1,gal_size):
+    systems = []
     for y in tqdm(range(6,497)):
         for z in range(1,16):
             pos=[gal,y,z,int(1)]
@@ -34,12 +36,21 @@ for gal in range(1,gal_size):
             for x in distros_dict:
                 if str(x["planet_position"]) == pos_check:
                     if x["player_rank"] is not None:
-                        if int(x["player_rank"]) < minimum_rank:
-                            planets = planets + 1
-                            if int(x["player_rank"]) < maximum_rank:
-                                planets = maximum_rank - int(x["player_rank"])
+                        if str(x["player_status"]) != "['vacation']":
+                            if int(x["player_rank"]) < minimum_rank:
+                                planets = planets + 1
+                                if int(x["player_rank"]) < maximum_rank:
+                                    planets = planets + (maximum_rank - int(x["player_rank"]))
+                                #print(int(x["player_rank"]), "so planets are: ", planets, "galaxy is: ", gal, " system is: ", y )
+                        elif ingore_vacat==0:
+                            if int(x["player_rank"]) < minimum_rank:
+                                planets = planets + 1
+                                #print(str(x["player_name"])," on vacation")
+                                if int(x["player_rank"]) < maximum_rank:
+                                    planets = planets + (maximum_rank - int(x["player_rank"]))
                     else:
                         player_rank = None
+
         #print(y, "has: ",planets)
         systems.append((y, planets))
         planets=0
@@ -57,6 +68,6 @@ for gal in range(1,gal_size):
     print("The smallest crowded systems in galaxy ",gal)
     for i in range(results):
         print(sums[i][0], sums[i][1])
-
 sleep(3)
+
 exec(open('Launch.py').read())
